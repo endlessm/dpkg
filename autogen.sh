bb0d44aabf7414f8c286aac2fab77caf66a56b68
Dan Nicholson <nicholson@endlessm.com>
2015-01-26 12:54:30 -0800

    build: Add autogen.sh for bootstrapping
    Standard autoreconf usage to bootstrap the repo in jenkins. This needs
    to be included in dist since dpkg expects everything from git except
    .gitignore to be distributed.

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+PROJECT=dpkg
+builddir=`pwd`
+srcdir=`dirname "$0"`
+[ -z "$srcdir" ] && srcdir=.
+
+# Rebuild the autotools
+cd "$srcdir"
+${AUTORECONF-autoreconf} -iv || exit $?
+cd "$builddir"
+
+# Run configure unless NOCONFIGURE set.
+if [ -z "$NOCONFIGURE" ]; then
+    "$srcdir"/configure "$@"
+    echo "Now type 'make' to compile $PROJECT."
+else
+    echo "Now type './configure && make' to compile $PROJECT."
+fi
